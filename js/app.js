16da543f5a1a06eb75f3eff9480fc266ce07c167
jazzcodes <jasleenbhambra01@gmail.com>
2023-05-15 19:43:42 +0530

    feat: to-do status management, delete, UI

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-// import { check } from 'prettier';
+import { createClient } from 'https://cors-anywhere.herokuapp.com/https://esm.sh/@supabase/supabase-js@2'
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dnlteW51aHZ0dnhoaHV4ZW9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4MDQ5NjQsImV4cCI6MTk5OTM4MDk2NH0.glQRUBx4EcVkh96LnCrxcoP0OnmSAqhk3c9cswgMG5U";
 const url = "https://oxvymynuhvtvxhhuxeoi.supabase.co";
@@ -9,37 +8,106 @@ console.log(supabase);
 
 // To-do
 
-let inputTask = document.getElementById("add-task");
+const inputTask = document.getElementById("add-task");
 const tasksList = document.querySelector(".tasks");
+
+
 
 
 // fetch data from supabase & display
 
 async function loadData() {
-    let { data: todo, error } = await supabase.from("todo").select("*");
+
+    const { data: todo, error } = await supabase.from("todo").select("*");
     console.log(error);
     console.log(todo);
+
+
 
     todo.forEach((task) => {
         console.log(task.id);
         console.log(task.task);
+
+        //display data
+
         tasksList.innerHTML += `
-        <li id=${task.id} class="task"> <div class="task-check"><input type="checkbox" class="done" name="${task.id}" id=""> <p class="strikethrough"> ${task.task} </p> </div><button id="delete-task" name=${task.id}>Delete</button></li>
+        <li id=${task.id} class="task"> <div class="task-check"><input type="checkbox" class="done" name="${task.id}" id=""> <p class="strikethrough"> ${task.task} </p> </div><button class="delete-task" name=${task.id}><img src="../images/delete-btn.svg" alt="" class="del-icon"> </button></li>
          
         `;
 
 
+        // handle delete
+
+        const deleteBtns = document.querySelectorAll(".delete-task");
+
+        deleteBtns.forEach((del) => {
+            del.addEventListener("click", async function handleDelete() {
+                console.log("deleted", del.name);
+                const { data: delData, delError } = await supabase
+                    .from('todo')
+                    .delete()
+                    .eq('id', del.name);
+
+                location.reload();
+
+            });
 
 
+
+        });
+
+
+
+        // console.log(tasksList);
+        const doneTask = document.querySelectorAll(".task .task-check .done"); // checkboxes
+        console.log(doneTask);
+
+        async function handleCheck() {
+
+            console.log(tasksList);
+            const taskItem = document.querySelectorAll(".task .task-check .done");
+            console.log(taskItem);
+
+            const { data: todoNew, errorNew } = await supabase.from("todo").select("id").eq("done", 'true');
+            console.log(errorNew);
+            console.log(todoNew);
+
+            const todoNew2 = todoNew.map(a => a.id);
+
+
+            taskItem.forEach((t) => {
+                console.log(todoNew2);
+                console.log("true-btn-name", t.name);
+                if (todoNew2.includes(Number(t.name))) {
+                    console.log(t.name, "is there");
+                    t.checked = true;
+                }
+
+                else {
+                    console.log("not there");
+                }
+            }
+            );
+
+
+
+
+
+        }
         // done status
-        const doneTask = document.querySelectorAll(".done"); // checkboxes
-        const strikethrough = document.querySelectorAll(".strikethrough");
         doneTask.forEach((done) => {
+            console.log(done);
+
             done.addEventListener("change", () => {
 
+
+                console.log("clicked");
                 console.log(done.checked);
                 const taskID = done.name;
+                task.done = done.checked;
+
                 console.log("status changed ID:", taskID);
+
 
                 async function handleDone() {
                     const { data: doneData, error: doneError } = await supabase
@@ -51,82 +119,39 @@ async function loadData() {
 
                 }
 
-
                 handleDone();
-                // strikethrough.forEach((p) => {
-                //     if (task.done) {
-                //         p.style.textDecoration = "line-through";
-                //         p.style.color = "white";
-                //     }
-                //     else {
-                //         p.style.textDecoration = "none";
-                //         p.style.color = "black";
-                //     }
-                // });
+                // handleCheck();
+
+                console.log("new status", task.done);
 
 
             });
 
-        }
 
 
-        );
 
-        // console.log(task.done);
-
-        const deleteBtn = document.querySelectorAll("#delete-task");
-        // console.log(deleteBtn);
-
-
-        deleteBtn.forEach((del) => {
-            del.addEventListener("click", async function handleDelete(e) {
-
-                e.preventDefault();
-
-                const { data: deleteData, error: deleteError } = await supabase
-                    .from('todo')
-                    .delete()
-                    .eq('id', deleteBtn.name);
-
-                console.log("deleted");
-
-                const task = document.querySelectorAll(".task");
-                task.forEach((t) => {
-                    if (t.id == deleteBtn.name) {
-                        taskList.remove(task);
-                    }
-
-                });
-
-
-            });
+            handleCheck();
 
 
         });
 
-    }
 
 
-    );
+
+
+    });
+
+
+
+
+
 
     inputTask.value = "";
-
-    // // Strikethrough
-
-    // const doneTask = document.querySelectorAll(".done");
-
-
-
-
-
-
-
 
 
 
 
 }
-
 
 
 // insert data to supabase
@@ -147,9 +172,8 @@ inputTask.addEventListener("keypress", async function insertData(event) {
 });
 
 
+
 loadData();
-
-
 
 
 
