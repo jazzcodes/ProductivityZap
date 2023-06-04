e7648b4d5d03026834585103d4dddb038338a217
jazzcodes <jasleenbhambra01@gmail.com>
2023-06-04 15:13:43 +0530

    fix: netlify cli 1.0

--- js/additional.js
+++ js/additional.js
@@ -1,27 +1,76 @@
-import { process } from '../lib/env.js';
+import { supabase } from '../lib/client.js';
 
 const chatbotInput = document.getElementById('chatbot-input');
 const chatbotResponse = document.querySelector('.chatbot-response');
 
-const apiKey = process.env.OPENAI_API_KEY;
-const url = 'https://api.openai.com/v1/completions';
+// const apiKey = process.env.OPENAI_API_KEY;
 
 function fetchBotReply() {
+  const url =
+    'https://luxury-hotteok-fb50c4.netlify.app/.netlify/functions/fetchAI';
   fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${apiKey}`,
+      // Authorization: `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
       model: 'text-davinci-003',
-      prompt: `Affirm the user that the ${chatbotInput.value} would be done positively.`,
+      prompt: `Behave like a friendly chatbot. Whenever user asks to do ${chatbotInput.value}, 
+      affirm the user that it would be done positively.`,
       max_tokens: 50,
     }),
   })
     .then((response) => response.json())
     .then((data) => (chatbotResponse.innerText = data.choices[0].text.trim()));
+  let userId = localStorage.getItem('userId');
+
+  if (
+    chatbotInput.value.includes('todo') &&
+    chatbotInput.value.includes('add')
+  ) {
+    const taskAdded = chatbotInput.value
+      .match(/"(\\.|[^"\\])*"/g)[0]
+      .replace(/['"]+/g, '');
+
+    async function addTaskAI() {
+      const { data: todo, error } = await supabase.from('todo').insert([
+        {
+          task: taskAdded,
+          user_id: userId,
+        },
+      ]);
+    }
+    addTaskAI();
+  }
+
+  if (
+    chatbotInput.value.includes('note') &&
+    chatbotInput.value.includes('add')
+  ) {
+    const noteHeadingAdded = chatbotInput.value
+      .match(/"(\\.|[^"\\])*"/g)[0]
+      .replace(/['"]+/g, '');
+
+    const noteContentAdded = chatbotInput.value
+      .match(/"(\\.|[^"\\])*"/g)[1]
+      .replace(/['"]+/g, '');
+
+    async function addNoteAI() {
+      const { data: noteData, error: noteError } = await supabase
+        .from('notes')
+        .insert([
+          {
+            heading: noteHeadingAdded,
+            content: noteContentAdded,
+            user_id: userId,
+          },
+        ]);
+    }
+    addNoteAI();
+  }
   chatbotInput.value = '';
+  chatbotResponse.style.display = 'initial';
 }
 
 chatbotInput.addEventListener('keypress', function (event) {
